753c1c2e2c2043f9ae3654bee17ce503ac1bf800
Nabil Labrazi <na.labrazi@gmail.com>
2021-01-17 09:21:33 +0100

    in progress

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,21 +1,13 @@
 class WatchesController < ApplicationController
 
   def new
-    @watch = Watch.new(watch_params)
-    # @brand = Brand.find(params[:brand_id])
+    @watch = Watch.new
+    @watch.brand = brand_id
+    @watch.model = model_id
   end
 
   def create
-    @watch = Watch.find(params[:watch_id])
-    if @watch.save
-      # Deliver the signup email
-      #NotificationMailer.create_project(@estimate).deliver
-      #redirect_to watch_path(@watch, :notice => 'RDV créé')
-      redirect_to root_path :notice => 'RDV Créé'
-    else
-      #redirect_to watch_path(@watch, :notice => 'Echec de la création de RDV')
-      redirect_to root_path :notice => 'Echec de la création de RDV'
-    end
+
   end
 
   def set_watch
